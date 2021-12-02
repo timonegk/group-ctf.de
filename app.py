b8402542eec5eff5b3dc5eeac017d0aa0d9f5e28
Timon Engelke <timon.engelke@online.de>
2021-12-03 00:13:07 +0100

    Fix code formatting using black and isort

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
 import os
 from pathlib import Path
 
-from flask import Flask, render_template, abort
+from flask import Flask, abort, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    path = 'content' / Path('index.md')
+    path = "content" / Path("index.md")
     md_text = path.read_text()
-    return render_template('content.md', md_text=md_text)
-
+    return render_template("content.md", md_text=md_text)
 
 
 @app.route("/<path:path_str>/")
 def content(path_str):
-    path = 'content' / Path(path_str)
+    path = "content" / Path(path_str)
     if path.is_dir():
-        path = path / 'index.md'
+        path = path / "index.md"
     else:
-        path = 'content' / Path(path_str + '.md')
+        path = "content" / Path(path_str + ".md")
     if not path.is_file():
         abort(404)
     # Assert file is in content directory
-    if Path('content').resolve() not in path.resolve().parents:
+    if Path("content").resolve() not in path.resolve().parents:
         abort(404)
     md_text = path.read_text()
-    return render_template('content.md', md_text=md_text)
+    return render_template("content.md", md_text=md_text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
