6f2febb6c4bca11ce7dcc6f1a31775cceb407760
Finn-Thorben Sell <dev@finn-thorben.me>
2021-12-07 22:29:19 +0100

    make paths irrelevant to working directory

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
-import os
 from pathlib import Path
 
 from flask import Flask, abort, render_template
+
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    path = "content" / Path("index.md")
+    path = BASE_DIR / "content" / "index.md"
     md_text = path.read_text()
     return render_template("content.md", md_text=md_text)
 
 
 @app.route("/<path:path_str>/")
 def content(path_str):
-    path = "content" / Path(path_str)
+    path = BASE_DIR / "content" / path_str
     if path.is_dir():
         path = path / "index.md"
     else:
-        path = "content" / Path(path_str + ".md")
+        path = BASE_DIR / "content" / (path_str + ".md")
     if not path.is_file():
         abort(404)
     # Assert file is in content directory
-    if Path("content").resolve() not in path.resolve().parents:
+    if BASE_DIR / "content" not in path.parents:
         abort(404)
     md_text = path.read_text()
     return render_template("content.md", md_text=md_text)
